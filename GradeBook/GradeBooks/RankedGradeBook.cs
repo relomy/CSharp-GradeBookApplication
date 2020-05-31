3917766c5e8de29fe6c022772cb4ffa21f7ce8cf
Adam Lewandowski <relomy@gmail.com>
2020-05-31 13:03:49 -0400

    that took way longer than it should have. added code to get letter grade for RankedGradeBook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,44 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            // ranked grading requires five or more students
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var gradeRank = 0;
+            foreach (var student in Students.OrderBy(s => s.AverageGrade))
+            {
+                if (averageGrade > student.AverageGrade)
+                {
+                    gradeRank++;
+                }
+            }
+
+            var gradeRankPercentage = (double)gradeRank / Students.Count;
+            if (gradeRankPercentage >= 0.8)
+            {
+                return 'A';
+            }
+            else if (gradeRankPercentage >= 0.6 && gradeRankPercentage < 0.8)
+            {
+                return 'B';
+            }
+            else if (gradeRankPercentage >= 0.4 && gradeRankPercentage < 0.6)
+            {
+                return 'C';
+            }
+            else if (gradeRankPercentage >= 0.2 && gradeRankPercentage < 0.4)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
