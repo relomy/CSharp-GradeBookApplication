940763e45855e7aa17610ab2708d2f072cab2f6a
Adam Lewandowski <relomy@gmail.com>
2020-05-31 13:42:38 -0400

    added isWeighted grading to gradebooks

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +109,35 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+
+            var gpaPoints = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpaPoints = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    gpaPoints = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpaPoints = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpaPoints = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpaPoints = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted &&
+                (studentType == StudentType.Honors ||
+                studentType == StudentType.DualEnrolled))
+            {
+                gpaPoints++;
+            }
+
+            return gpaPoints;
         }
 
         public virtual void CalculateStatistics()
